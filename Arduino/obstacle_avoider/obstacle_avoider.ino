fca2862970b1747ebf76796e5e3f9ab4d6cbe6a8
Avirup Basu <ami.avirup17@gmail.com>
2022-05-29 23:42:45 +0530

    Updated logic

--- Arduino/obstacle_avoider/obstacle_avoider.ino
+++ Arduino/obstacle_avoider/obstacle_avoider.ino
@@ -1,77 +1,26 @@
-//Include pin mappings for the motor driver
-/*
- * IN1=7
- * IN2=8
- * IN3=9
- * IN4=10
- */
-
 //User Macros to defin pin numbers
-#define in1 7;
-#define in2 8;
-#define in3 9;
-#define in4 10;
+
+
+//Use global variables for distance, duration and threshold distance
 
 void setup()
 {
   //Initialise the pins as output
-  pinMode(in1,OUTPUT);
-  pinMode(in1,OUTPUT);
-  pinMode(in1,OUTPUT);
-  pinMode(in1,OUTPUT);  
+
+  //Initialise distance measurement pins
+
+  //Initialise Serial port communication
+ 
 }
 
-void forward()
-{
-  digitalWrite(in1,HIGH);
-  digitalWrite(in1,LOW);
-  digitalWrite(in1,HIGH);
-  digitalWrite(in1,LOW);
-}
-
-void backward()
-{
-  digitalWrite(in1,LOW);
-  digitalWrite(in1,HIGH);
-  digitalWrite(in1,LOW);
-  digitalWrite(in1,HIGH);
-}
-
-void left()
-{
-  digitalWrite(in1,LOW);
-  digitalWrite(in1,HIGH);
-  digitalWrite(in1,HIGH);
-  digitalWrite(in1,LOW);
-  
-}
-
-void right()
-{
-  digitalWrite(in1,HIGH);
-  digitalWrite(in1,LOW);
-  digitalWrite(in1,LOW);
-  digitalWrite(in1,HIGH);
-  
-}
-
-void halt()
-{
-  digitalWrite(in1,LOW);
-  digitalWrite(in1,LOW);
-  digitalWrite(in1,LOW);
-  digitalWrite(in1,LOW);
-  
-}
 
 void loop()
 {
-  forward();
-  delay(5000);
-  backward();
-  delay(5000);
-  left();
-  delay(5000);
-  right();
-  delay(5000);
+  //Calculate distance
+
+  //if distance<threshold
+  //perform left or right turn
+
+  //else
+  //go straight
 }
