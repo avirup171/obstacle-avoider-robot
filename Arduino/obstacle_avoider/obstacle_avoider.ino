ca951c8d9b1e43af9f87dbf22041eaf688af8a8b
Avirup Basu <ami.avirup17@gmail.com>
2022-05-30 12:20:58 +0530

    Updated codebase

--- Arduino/obstacle_avoider/obstacle_avoider.ino
+++ Arduino/obstacle_avoider/obstacle_avoider.ino
@@ -1,26 +1,82 @@
+//PIN MAPPING
+/*
+
+* IN1               = 7 
+* IN2               = 8
+* IN3               = 9
+* IN4               = 10
+* HCSR04 TRIGGER    = 5
+* HCSR04 ECHO       = 4
+* SERVO             = 3
+
+*/
+
+
 //User Macros to defin pin numbers
 
+//Motor pins
 
-//Use global variables for distance, duration and threshold distance
+//Servo pins
+
+//Ultrasound pins
+
+//Attach servo pin
+
+
+//Use global variables 
+float instantDistance=0.0;
+
+//Set the threshold distance. Keep it 10 cms
+float threshold = 10.0;
 
 void setup()
 {
-  //Initialise the pins as output
+  //Set motor pins as output
 
-  //Initialise distance measurement pins
+  //Set trigger pin as output
 
-  //Initialise Serial port communication
+  //Set echoo pin as input
+
+  //Attach servo pin
  
 }
 
 
 void loop()
 {
-  //Calculate distance
 
-  //if distance<threshold
-  //perform left or right turn
+  //Implement main logic here
 
-  //else
-  //go straight
+
 }
+
+void forward()
+{
+
+}
+
+void backward()
+{
+
+}
+
+void halt()
+{
+
+}
+
+void left()
+{
+
+}
+
+void right()
+{
+
+}
+
+float getDistance()
+{
+  return 1.0;
+}
+
